980767959e4b69ebdf6f63df6783cad63fdeef81
Marcos Ridao <marcoss.ridao@gmail.com>
2022-09-09 21:14:51 -0300

    Arreglo errores en confirmar_rendicion

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import sys
 
 
 def main():
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cajachica.settings.production')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cajachica.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cajachica.settings.production')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cajachica.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
